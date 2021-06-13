2fd9e3f8c2e85cdb40edc845f68b07ed4f5a38eb
Hrukem <junghe@yandex.ru>
2021-06-14 00:12:42 +0300

    change config.exs

--- config/config.exs
+++ config/config.exs
@@ -21,8 +21,8 @@ config :weither, WeitherWeb.Endpoint,
 # Configures Quantum
 config :weither, Weither.Scheduler,
   jobs: [
-    {"0 */3 * * *", fn -> Weither.Api.get(:weather) end},
-    {"1 0 * * *", fn -> Weither.Cache.Forecast.init() end}
+    {"0 */3 * * *", fn -> {Weither.Api, :get, [:weather]} end},
+    {"1 0 * * *", fn -> {Weither.Cache.Forecast, :init, []} end}
   ]
 
 # Configures Elixir's Logger
