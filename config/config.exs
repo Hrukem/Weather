274c97b9c5b6b35d720d8603e7730b7e33e7b0b9
Hrukem <junghe@yandex.ru>
2021-06-14 19:48:12 +0300

    modif config

--- config/config.exs
+++ config/config.exs
@@ -14,7 +14,7 @@ config :weither, Weither.Repo,
   username: "postgres",
   password: "postgres",
   database: "weither_dev",
-  hostname: 172.17.0.2,
+  hostname: "host.docker.internal",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
